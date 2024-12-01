9e678ba2cefa19b0a47ec4003a36a1184b893da5
Chiffa <artemy040404@gmail.com>
2024-12-01 23:54:30 +0300

    d1: end of basic parsing optimisations

--- src/day1/part1.rs
+++ src/day1/part1.rs
@@ -1,30 +1,36 @@
 use std::iter::zip;
 
-#[aoc(day1, part1)]
+// #[aoc(day1, part1)]
 pub fn run(input: &str) -> usize {
-    let mut left = Vec::with_capacity(1000);
-    let mut right = Vec::with_capacity(1000);
+    let mut left = [0; 1000];
+    let mut right = [0; 1000];
     input
         .split('\n')
-        .map(|line| unsafe {
-            (
-                parse_digit_unrolled(&line[0..5]),
-                parse_digit_unrolled(&line[8..13]),
-            )
-        })
-        .for_each(|(lhs, rhs)| {
-            left.push(lhs);
-            right.push(rhs);
+        .map(|line| unsafe { parse_two_numbers_unrolled(line) })
+        .enumerate()
+        .for_each(|(idx, (lhs, rhs))| {
+            left[idx] = lhs;
+            right[idx] = rhs;
         });
     left.sort_unstable();
     right.sort_unstable();
 
     zip(left, right).fold(0, |acc, (lhs, rhs)| acc + lhs.abs_diff(rhs))
 }
-unsafe fn parse_digit_unrolled(s: &str) -> usize {
-    (*s.as_bytes().get_unchecked(0) as usize - b'0' as usize) * 10000
-        + (*s.as_bytes().get_unchecked(1) as usize - b'0' as usize) * 1000
-        + (*s.as_bytes().get_unchecked(2) as usize - b'0' as usize) * 100
-        + (*s.as_bytes().get_unchecked(3) as usize - b'0' as usize) * 10
-        + (*s.as_bytes().get_unchecked(4) as usize - b'0' as usize) * 1
+#[inline(always)]
+unsafe fn parse_two_numbers_unrolled(s: &str) -> (usize, usize) {
+    (
+        // Left number
+        (*s.as_bytes().get_unchecked(0) as usize - b'0' as usize) * 10000
+            + (*s.as_bytes().get_unchecked(1) as usize - b'0' as usize) * 1000
+            + (*s.as_bytes().get_unchecked(2) as usize - b'0' as usize) * 100
+            + (*s.as_bytes().get_unchecked(3) as usize - b'0' as usize) * 10
+            + (*s.as_bytes().get_unchecked(4) as usize - b'0' as usize) * 1,
+        // Right number
+        (*s.as_bytes().get_unchecked(8) as usize - b'0' as usize) * 10000
+            + (*s.as_bytes().get_unchecked(9) as usize - b'0' as usize) * 1000
+            + (*s.as_bytes().get_unchecked(10) as usize - b'0' as usize) * 100
+            + (*s.as_bytes().get_unchecked(11) as usize - b'0' as usize) * 10
+            + (*s.as_bytes().get_unchecked(12) as usize - b'0' as usize) * 1,
+    )
 }
--- src/day1/part2.rs
+++ src/day1/part2.rs
@@ -1,21 +1,15 @@
 // #[aoc(day1, part2)]
 pub fn run(input: &str) -> usize {
-    let mut left = Vec::with_capacity(1000);
-    let mut right = Vec::with_capacity(1000);
+    let mut left = [0; 1000];
+    let mut right_freq: [usize; 100000] = [0; 100000];
     input
         .split('\n')
-        .map(|line| unsafe {
-            (
-                parse_digit_unrolled(&line[0..5]),
-                parse_digit_unrolled(&line[8..13]),
-            )
-        })
-        .for_each(|(lhs, rhs)| {
-            left.push(lhs);
-            right.push(rhs);
+        .map(|line| unsafe { parse_two_numbers_unrolled(line) })
+        .enumerate()
+        .for_each(|(idx, (lhs, rhs))| {
+            left[idx] = lhs;
+            right_freq[rhs] += 1;
         });
-    let mut right_freq: [usize; 100000] = [0; 100000];
-    right.into_iter().for_each(|item| right_freq[item] += 1);
 
     left.iter()
         .fold(0, |acc: usize, item| acc + *item * right_freq[*item])
@@ -27,4 +21,21 @@ unsafe fn parse_digit_unrolled(s: &str) -> usize {
         + (*s.as_bytes().get_unchecked(2) as usize - b'0' as usize) * 100
         + (*s.as_bytes().get_unchecked(3) as usize - b'0' as usize) * 10
         + (*s.as_bytes().get_unchecked(4) as usize - b'0' as usize) * 1
+}
+#[inline(always)]
+unsafe fn parse_two_numbers_unrolled(s: &str) -> (usize, usize) {
+    (
+        // Left number
+        (*s.as_bytes().get_unchecked(0) as usize - b'0' as usize) * 10000
+            + (*s.as_bytes().get_unchecked(1) as usize - b'0' as usize) * 1000
+            + (*s.as_bytes().get_unchecked(2) as usize - b'0' as usize) * 100
+            + (*s.as_bytes().get_unchecked(3) as usize - b'0' as usize) * 10
+            + (*s.as_bytes().get_unchecked(4) as usize - b'0' as usize) * 1,
+        // Right number
+        (*s.as_bytes().get_unchecked(8) as usize - b'0' as usize) * 10000
+            + (*s.as_bytes().get_unchecked(9) as usize - b'0' as usize) * 1000
+            + (*s.as_bytes().get_unchecked(10) as usize - b'0' as usize) * 100
+            + (*s.as_bytes().get_unchecked(11) as usize - b'0' as usize) * 10
+            + (*s.as_bytes().get_unchecked(12) as usize - b'0' as usize) * 1,
+    )
 }
